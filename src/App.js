449013827c8c2bf4dc10c8c617fa764bd47c9d08
Amr Waheed <amrmohamed877@gmail.com>
2024-11-17 07:20:28 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -331,7 +331,10 @@ function WatchedMovie({ movie,onDeleteWatched }) {
           <span>⭐️</span>
           <span>{movie.imdbRating}</span>
         </p>
-
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
         <p>
           <span>⏳</span>
           <span>{movie.runtime} min</span>
