a794f68d11e3a6137ed5d9956b05bb72bb455a21
Amr Waheed <amrmohamed877@gmail.com>
2024-11-27 20:54:02 +0200

    minor changes

--- src/Components/WatchedList.jsx
+++ src/Components/WatchedList.jsx
@@ -1,10 +1,11 @@
 import WatchedMovie from './WatchedMovie'
 
+const key=crypto.randomUUID()
 function WatchedList({ watched, handleDeleteWatched }) {
     return (
         <ul className="list">
             {watched.map((movie) => (
-                <WatchedMovie onDeleteWatched={handleDeleteWatched} movie={movie} key={movie.imdbID} />
+                <WatchedMovie onDeleteWatched={handleDeleteWatched} movie={movie} key={key} />
             ))}
         </ul>
     )
